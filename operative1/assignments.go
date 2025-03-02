d5ca1b12037fab3ea92ef3b752168dec32add943
advanced-go <mark.wilson.go@outlook.com>
2025-03-02 16:18:16 -0600

    Update assignments.go

--- operative1/assignments.go
+++ operative1/assignments.go
@@ -2,18 +2,17 @@ package operative1
 
 import (
 	"github.com/behavioral-ai/core/messaging"
+	"github.com/behavioral-ai/domain/common"
 )
 
 func createAssignments(agent *ops, newAgent newOfficerAgent) {
-	a := newAgent(westOrigin, agent.resolver, agent.dispatcher)
+	addAssignment(agent, westOrigin, newAgent)
+	addAssignment(agent, centralOrigin, newAgent)
+}
+
+func addAssignment(agent *ops, origin common.Origin, newAgent newOfficerAgent) {
+	a := newAgent(origin, agent.resolver, agent.dispatcher)
 	err := agent.caseOfficers.Register(a)
-	if err != nil {
-		agent.resolver.Notify(messaging.NewStatusError(messaging.StatusInvalidArgument, err, agent.Uri()))
-	} else {
-		a.Run()
-	}
-	a = newAgent(centralOrigin, agent.resolver, agent.dispatcher)
-	err = agent.caseOfficers.Register(a)
 	if err != nil {
 		agent.resolver.Notify(messaging.NewStatusError(messaging.StatusInvalidArgument, err, agent.Uri()))
 	} else {
