338c6b3e55ed0fec9e5f2e2c10048f7204684434
advanced-go <mark.wilson.go@outlook.com>
2025-03-15 11:35:30 -0500

    Update package.go

--- module/package.go
+++ module/package.go
@@ -1,19 +1,12 @@
 package module
 
 import (
-	"github.com/behavioral-ai/collective/content"
 	"github.com/behavioral-ai/collective/test"
 	"github.com/behavioral-ai/core/messaging"
 	"github.com/behavioral-ai/operations/operative1"
 )
 
-const (
-	Domain         = "github/behavioral-ai/operations"
-	ResiliencyPath = "/resiliency"
-)
-
-func Startup(hosts []string, do content.HttpExchange, appHostName string) {
-	content.Startup(hosts, do, appHostName)
+func Startup(hostName string) {
 	test.Startup()
 	AgentMessage(messaging.StartupEvent)
 }
